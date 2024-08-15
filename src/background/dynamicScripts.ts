ce5cbbeb74b68d985939003c1fad1d518c26ba68
aadilmallick <aadiltj.mallick@gmail.com>
2024-08-15 15:21:46 -0400

    finally completed project

--- src/background/dynamicScripts.ts
+++ src/background/dynamicScripts.ts
@@ -39,6 +39,135 @@ export async function onColorPick(tabId: number, url: string) {
       button.style.top = "10px";
       button.style.left = "10px";
       button.style.zIndex = "100000";
+      type ToastType = "info" | "success" | "danger" | "default" | "warning";
+      type ToastManagerOptions = {
+        timeout?: number;
+        position?: "top-right" | "top-left" | "bottom-right" | "bottom-left";
+      };
+      class ToastManager {
+        private toastContainer?: HTMLElement;
+        private options: ToastManagerOptions;
+
+        public get containerElement() {
+          return this.toastContainer;
+        }
+
+        constructor(options: ToastManagerOptions = {}) {
+          this.options = {
+            timeout: 3000,
+            position: "bottom-right",
+            ...options,
+          };
+        }
+
+        setup() {
+          const element = document.getElementById("toasts");
+          if (element) return;
+          const toastContainer = document.createElement("div");
+          toastContainer.id = "toasts";
+          toastContainer.classList.add(this.options.position!);
+
+          // add toast container to body
+          document.body.appendChild(toastContainer);
+
+          // set toast container
+          this.toastContainer = toastContainer;
+        }
+
+        toast(message: string, type: ToastType = "default") {
+          const toast = new Toast({
+            message,
+            type,
+            duration: this.options.timeout,
+          });
+          if (!this.toastContainer) throw new Error("Toast container not set");
+          this.toastContainer.appendChild(toast.element);
+          toast.show();
+        }
+
+        success(message: string) {
+          this.toast(message, "success");
+        }
+
+        info(message: string) {
+          this.toast(message, "info");
+        }
+
+        danger(message: string) {
+          this.toast(message, "danger");
+        }
+
+        warning(message: string) {
+          this.toast(message, "warning");
+        }
+      }
+
+      class Toast {
+        public element: HTMLElement;
+        private timeoutId: number | null = null;
+        private duration: number;
+        constructor({
+          message,
+          duration = 3000,
+          type = "default",
+          onClick,
+        }: {
+          message: string;
+          type?: ToastType;
+          duration?: number;
+          onClick?: () => void;
+        }) {
+          const toast = document.createElement("div");
+          toast.classList.add("toast");
+          toast.classList.add(`toast-${type}`);
+          toast.style.setProperty("--toast-duration", `${duration}ms`);
+          toast.innerText = message;
+
+          this.element = toast;
+          this.duration = duration;
+
+          this.element.addEventListener(
+            "click",
+            (e) => {
+              e.stopPropagation();
+              onClick?.();
+              this.dismiss();
+            },
+            {
+              once: true,
+            }
+          );
+        }
+
+        animateOut() {
+          const animation = this.element.animate(
+            [{ opacity: 0, transform: "translateX(250px)" }],
+            {
+              duration: 250,
+            }
+          );
+          // wait for animation to finish
+          animation.onfinish = () => {
+            this.element.remove();
+          };
+        }
+
+        show() {
+          this.timeoutId = setTimeout(() => {
+            this.animateOut();
+            this.timeoutId = null;
+          }, this.duration);
+        }
+
+        dismiss() {
+          if (this.timeoutId) {
+            clearTimeout(this.timeoutId);
+            this.timeoutId = null;
+            this.animateOut();
+          }
+        }
+      }
+
       class EyedropperManager {
         private abortController = new AbortController();
         private cb!: (event: KeyboardEvent) => void;
@@ -74,12 +203,18 @@ export async function onColorPick(tabId: number, url: string) {
       }
 
       const eyedropperManager = new EyedropperManager();
+      const toastManager = new ToastManager();
+      toastManager.setup();
       return new Promise((resolve) => {
         button.addEventListener(
           "click",
           async () => {
             button.style.display = "none";
             const color = await eyedropperManager.getColor();
+            if (color) {
+              toastManager.success("Color copied!");
+              await navigator.clipboard.writeText(color);
+            }
             resolve(color);
           },
           { once: true }
